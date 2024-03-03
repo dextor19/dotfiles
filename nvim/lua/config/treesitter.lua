28fdf4b78113d32f9188e8fec3a88a90c4d2baa4
Dexter Griffiths <dexgriffiths@hotmail.co.uk>
2024-03-04 01:36:43 +0900

    feat(treesitter): include more syntax highlighting for basic languages

--- nvim/lua/config/treesitter.lua
+++ nvim/lua/config/treesitter.lua
@@ -2,13 +2,11 @@ local M = {}
 
 function M.setup()
   require('nvim-treesitter.configs').setup({
-    ensure_installed = { 'c', 'cpp', 'go', 'nix', 'python', 'rust', 'lua' },
-    sync_install = false,
+    ensure_installed = { 'c', 'cpp', 'go', 'nix', 'python', 'rust', 'lua', 'vim', 'markdown', 'vimdoc', 'bash', 'html' },
+    auto_install = true,
+    highlight = { enable = true },
+    indent = { enable = true },
   })
-
-  -- Tree sitter folding
-  vim.opt.foldmethod = 'expr'
-  vim.opt.foldexpr = 'nvim_treesitter#foldexpr()'
 end
 
 return M
